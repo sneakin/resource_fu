8cd4fee2a98354a4860486cb93280f13e94e164a
trevor <trevor@53f05ecc-a62b-0410-82cc-ca94651364e2>
2007-05-02 22:40:45 +0000

    * Experimental: added default_template_path to controllers
    git-svn-id: http://svn.protocool.com/public/plugins/resource_fu@10 53f05ecc-a62b-0410-82cc-ca94651364e2

--- init.rb
+++ init.rb
@@ -1,4 +1,5 @@
 require 'proto_cool/resource_fu'
+require 'proto_cool/resource_fu/default_template_path'
 require 'proto_cool/resource_fu/method_definition'
 require 'proto_cool/resource_fu/named_route_collection_methods'
 require 'proto_cool/resource_fu/opaque_resource_names'
@@ -8,5 +9,7 @@ require 'proto_cool/resource_fu/helper_delegation'
 ActionController::Routing::RouteSet::NamedRouteCollection.send(:include, ProtoCool::ResourceFu::NamedRouteCollectionMethods)
 # add opaque_name argument to Resource
 ActionController::Resources::Resource.send(:include, ProtoCool::ResourceFu::OpaqueResourceNames)
+# add default_template_path to ActionController::Base
+ActionController::Base.send :include, ProtoCool::ResourceFu::DefaultTemplatePath
 # add helper delegation to ActionController::Base
 ActionController::Base.extend ProtoCool::ResourceFu::HelperDelegation
